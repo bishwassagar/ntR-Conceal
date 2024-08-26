56b4171841a655ad55bf7abeb89aa56bc205a519
bishwassagar <bishwas@tuta.io>
2024-08-26 08:59:36 +0530

    - 🔧 Added `vercel.json` for Vercel deployment and client-side routing rewrites
    🔄 Updated routing:
    - Refactored `App.jsx` for correct routing setup
    - Adjusted `main.jsx` for proper rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home.jsx';
-import './App.css';
-import Encryption from "./components/Encryption.jsx";
-import Decryption from "./components/Decryption.jsx";
+import Encryption from './components/Encryption.jsx';
+import Decryption from './components/Decryption.jsx';
 import About from './components/About.jsx';
 import PrivacyPolicy from './components/PrivacyPolicy.jsx';
+import './App.css';
 
 function App() {
   return (
-    <>
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/encrypt" element={<Encryption />} />
-        <Route path="/decrypt" element={<Decryption />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/encrypt" element={<Encryption />} />
+      <Route path="/decrypt" element={<Decryption />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
